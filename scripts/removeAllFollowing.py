5ff1bd9c94281bbebcb9193e0c22b848754689fa
josh-ua <oh.yosh@gmail.com>
2016-07-28 13:44:40 -0400

    updated info

--- scripts/removeAllFollowing.py
+++ scripts/removeAllFollowing.py
@@ -11,9 +11,9 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#
-#This script unfollows all follwing by the authed user.
-#
+########################################################
+#This script unfollows all follwing by the authed user.#
+########################################################
 
 def unfollow():
 	print()
