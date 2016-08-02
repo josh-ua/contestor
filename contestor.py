39805e2e6ddd056d754e518d1d7f650a090a0de0
josh-ua <oh.yosh@gmail.com>
2016-08-02 12:45:08 -0400

    Added shouldFollow and hasRetweet
    Checks to see if the tweet needs to be retweeted (broken right now - will probably scrap because I like that some normal tweets get through ;)) and checks to see if "follow" is in the tweet before following author.

--- contestor.py
+++ contestor.py
@@ -7,6 +7,7 @@ import tweepy, time, config
 
 
 
+
 #Imported from config.py (hidden from git)
 #auth stuff
 CONSUMER_KEY = config.CONSUMER_KEY
@@ -27,18 +28,12 @@ def main():
 	for searchTerm in searchList:
 		list = api.search(q=searchTerm, rpp = 50)
 		for tweet in list:
-			if tweet.in_reply_to_screen_name == None:
+			if tweet.in_reply_to_screen_name == None and hasRetweet(tweet) == True:
 				try:
 					api.retweet(tweet.id)
 					likeThis(tweet)
-					checkForFollow(tweet)
-					tweetedCount = tweetedCount + 1
-			
-					if (hasattr(tweet,'retweeted_status') == True): ##if a retweet, follow original tweeter
-						follow(tweet.retweeted_status.author.id)
-					else:
-						follow(tweet.author.screen_name)
-						
+					shouldFollow(tweet)
+					tweetedCount = tweetedCount + 1					
 				except:
 					notTweetedCount = notTweetedCount + 1
 					continue
@@ -47,7 +42,39 @@ def main():
 	print (str(notTweetedCount) + " tweets NOT tweeted.")
 	
 	
+
+#Checks if author should be followed (tweet contains following, follow, etc)	
+def shouldFollow(tweet):
+	followList = ["follow"]
+	tweetText = (tweet.text).lower() #'follow' should account for all variations of the world (e.g. 'following') as well..
 	
+	for term in followList:
+		if term in tweetText:
+			if (hasattr(tweet,'retweeted_status') == True): ##if a retweet, follow original tweeter
+				follow(tweet.retweeted_status.author.id)
+			else:
+				follow(tweet.author.screen_name)
+			
+			checkForExtraFollows(tweet)
+			return;
+			
+		else:
+			continue
+			
+			
+#Checks if the tweet has "retweet" or "RT" in it. Otherwise it's probably not a contest tweet.
+def hasRetweet(tweet):
+	retweetList = ["retweet", "rt"]
+	tweetText = (tweet.text).lower()
+	
+	for term in retweetList:
+		if term in tweetText:
+			return True
+		else:
+			continue
+	
+	return False
+		
 #Checks to see if the text in the tweet has the terms "like", "favorite", or "favourite" (for my British tweeters). 
 #If so, the tweet will be favorited. Some giveaways require this.
 def likeThis(tweet):
@@ -56,6 +83,7 @@ def likeThis(tweet):
 	for term in likeList:
 		if term in tweetText:
 			api.create_favorite(tweet.id)
+			return
 		else:
 			continue
 			
@@ -66,7 +94,7 @@ def likeThis(tweet):
 #TODO- punctuation needs to be stripped from handles
 #e.g. if tweet reads: "Follow @xxxx!!" function will fail and throw expection
 #also if tweet.author.screen_name == word, do not try to follow (optimize)
-def checkForFollow(tweet):
+def checkForExtraFollows(tweet):
 	try:
 		words = tweet.text.split()
 		for word in words:
@@ -74,8 +102,24 @@ def checkForFollow(tweet):
 						follow(word.replace('@',""))
 	except:  
 		return;
-	
 
+		
+		
+#Randomly tweet things to seem more human(?) - still working on
+#def clevTweet():
+	#trends = api.trends_place(2514815)
+	#data = trends[0]
+	#trends = data['trends']
+	#trendNames = [trend['name'] for trend in trends][0:5]
+
+	#choice = random.choice(trendNames)
+	#cbResponse = cb.ask(choice)
+	#print (cbResponse + " " + choice)
+
+
+	#if list size is zero, get new list of trends
+	#randomly pick one, take it out of the list
+	#ask cb, tweet response
 				
 				
 				
@@ -85,14 +129,14 @@ def follow(authorID):
 		api.create_friendship(authorID)
 		maintainFollowing()
 	except:
-		return ##already followed
-		
+		return
+
 	
 
 #Maintains followers (twitter has some following/follower ratio limit). This keeps the following from going over 1000.
 def maintainFollowing():
 	friendslist = api.friends_ids(config.USERNAME)
-	if  len(friendslist) > 1500:
+	if  len(friendslist) > 1300:
 		lastFollower = friendslist[len(friendslist) - 1]
 		api.destroy_friendship(lastFollower)
 		
