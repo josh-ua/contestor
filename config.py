2b1a2cfd0444408ed1c0aa8612bc80dcf075b105
josh-ua <oh.yosh@gmail.com>
2016-08-11 11:15:31 -0400

    added rest time interval setting

--- config.py
+++ config.py
@@ -6,3 +6,9 @@ ACCESS_SECRET = "xxxx"
 
 ##user information
 USERNAME = "xxxx"
+
+##settings
+#rest time interval (random amount of seconds picked between these numbers)
+#201 and 601 are defaults currently
+REST_TIME1 = 299
+REST_TIME2 = 601
